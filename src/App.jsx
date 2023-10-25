cbe80c849082463b01e1585470de4ad02f46f54e
lenienguyen <lenie.nguy10@gmail.com>
2023-10-25 22:26:56 +0200

    feat/cart: add cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React, { useContext, useState } from 'react'
-import ReactDOM from 'react-dom'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals';
-import Modal from './components/UI/Modal';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/cart-provider';
 import CartContext from './store/cart-context';
-import Checkout from './components/Cart/Checkout';
 
-
-// Todo:
-/*
- Add components for displaying :
-? OK - products
-- the cart (in a modal)
-- checkout form (in a modal)
-
-? OK Fetch the (dummy) meals data from the backend & show it on the screen (GET /meals)
-
-Allow users to add & remove products & show it on the screen (GET /meals)
-
-Send cart data along with user data (full name, email, street, postal code, city) to the backend (POST /orders)
-
-Handle loading & error states
-
-*/
 
 const App = () => {
   const cartCtx = useContext(CartContext)
-  const [showModal, setShowModal] = useState(false)
+  const [cartisShown, setCartIsShown] = useState(false)
 
   const onAddItemHandler = (item) => {
     cartCtx.addItem(item)
@@ -36,17 +16,13 @@ const App = () => {
 
   return (
     <CartProvider>
-
-      {/* {ReactDOM.createPortal(<Modal className="cart">
-        <Cart />
-      </Modal>, document.getElementById('modal'))}
-
-      {ReactDOM.createPortal(<Modal className="cart">
-        <Checkout />
-      </Modal>, document.getElementById('modal'))} */}
+      
+      {cartisShown && <Cart />}
 
       <Header />
+
       <Meals onClick={onAddItemHandler} />
+
     </CartProvider>
   );
 }
