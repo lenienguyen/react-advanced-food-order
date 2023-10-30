86d877ac7d7c4fc12d186b3307041faaee1178b9
lenienguyen <lenie.nguy10@gmail.com>
2023-10-30 16:50:10 +0100

    feat/context: add UserProgressContext for modal

---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react"
+
+// Context for modal
+const UserProgressContext = createContext({
+    progress: '', // 'cart' or 'checkout'
+    showCart: () => {},
+    hideCart: () => {},
+    showCheckout: () => {},
+    hideCheckout: () => {}
+})
+
+export function UserProgressContextProvider({children}){
+    const [userProgress, setUserProgress] = useState('');
+
+    const showCart = () => {
+        setUserProgress('cart')
+    }
+
+    const hideCart = () => {
+        setUserProgress('')
+    }
+
+    const showCheckout = () => {
+        setUserProgress('checkout')
+    }
+
+    const hideCheckout = () => {
+        setUserProgress('')
+    }
+
+    const userProgressContext = {
+        progress: userProgress,
+        showCart,
+        hideCart,
+        showCheckout,
+        hideCheckout
+    }
+
+    return <UserProgressContext.Provider value={userProgressContext}>{children}</UserProgressContext.Provider>
+}
+
+export default UserProgressContext
